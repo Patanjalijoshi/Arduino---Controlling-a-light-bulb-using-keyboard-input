9fcd3342e893c88357b52f63c51e8e178096d33c
Patanjali <joshipattu12@gmail.com>
2022-08-28 12:22:16 +0530

    Create Controlling_a_relay_with_keyboard_input.c.ino

---
+++ Controlling_a_relay_with_keyboard_input.c/Controlling_a_relay_with_keyboard_input.c.ino
@@ -0,0 +1,20 @@
+char inByte; // initialize the variable inByte
+# define relay 2 // pin that the LED is attached to
+
+void setup() {
+pinMode(relay, OUTPUT);
+Serial.begin(57600);
+
+}
+
+void loop() {
+  if(Serial.available()){
+    inByte = Serial.read();
+    if(inByte == 'y'){
+      digitalWrite(relay,HIGH);
+      }
+    if(inByte == 'n'){
+      digitalWrite(relay,LOW);
+      } 
+    }
+}
